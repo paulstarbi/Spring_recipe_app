e1778a43dee890d40d630710d49490a738b9e64a
Pawel Stabryla <paulstarbi@gmail.com>
2017-10-17 16:56:08 +0200

    spring-guru course - recepie app, simple index page with controller

---
+++ src/main/java/com/pauls/springrecipie/SpringRecipieApplication.java
@@ -0,0 +1,12 @@
+package com.pauls.springrecipie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipieApplication.class, args);
+	}
+}
